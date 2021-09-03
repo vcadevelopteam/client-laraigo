774309cefc901b7b1d312fc4eed0d7c99c9cce49
Angel Chumbes <angel.chumbes@vcaperu.com>
2021-09-03 18:21:21 -0500

    Modificacion de la vista de domios

--- src/common/helpers/requestBodies.ts
+++ src/common/helpers/requestBodies.ts
@@ -219,10 +219,10 @@ export const getDomainValueSel = (domainname: string): IRequestBody => ({
     }
 })
 
-export const insDomain = ({ domainname, operation }: Dictionary): IRequestBody => ({
+export const insDomain = ({ domainname, status, operation }: Dictionary): IRequestBody => ({
     method: "UFN_DOMAIN_INS",
     key: "UFN_DOMAIN_INS",
-    parameters: { id: 0, domainname, operation }
+    parameters: { id: 0, domainname, status, operation }
 });
 
 export const insDomainvalue = ({ id, domainname, description, domainvalue, domaindesc, status, type, bydefault, operation }: Dictionary): IRequestBody => ({
