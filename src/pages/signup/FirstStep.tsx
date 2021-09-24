e101bd8b627690444c0a59ffc1bfd12bc609ed68
Jose Vara <desarrollo.vca@vcaperu.com>
2021-09-24 11:38:58 -0500

    f

--- src/pages/signup/FirstStep.tsx
+++ src/pages/signup/FirstStep.tsx
@@ -28,6 +28,7 @@ import { useForm } from "react-hook-form";
 interface ChannelOption {
     icon: React.ReactNode;
     label: React.ReactNode;
+    key: string;
     selected: Boolean;
     onClick: () => void;
 }
@@ -119,11 +120,8 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
     const [mainData, setMainData] = useState<Dictionary>({
         email: ""
     });
-    const [error, setError] = useState<Dictionary>({
-        email: false
-    });
 
-    const [listchannels, setlistchannels] = useState({
+    const [listchannels, setlistchannels] = useState<Dictionary>({
         facebook:false,
         instagram: false,
         messenger: false,
@@ -136,13 +134,11 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
         phone: false,
         sms: false,
     });
+
     function maindataChange(field:string,value:any){
         let tempfield = mainData;
-        let temperror = error;
         tempfield[field] = value;
-        temperror[field] = value!!;
         setMainData(tempfield);
-        setError(temperror);
     }
     const dispatch = useDispatch();
     const [showPassword, setShowPassword] = useState(false);
@@ -152,71 +148,63 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
         {
             icon: <FacebookIcon color="inherit" />,
             label: 'Facebook',
+            key: 'facebook',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.facebook = !partiallist.facebook
-                setlistchannels(partiallist)
-                console.log(listchannels)
+                setlistchannels(p=>({...p,facebook:!p.facebook}))
             },
             selected: listchannels.facebook
         },
         {
             icon: <InstagramIcon color="inherit" />,
             label: 'Instagram',
+            key: 'instagram',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.instagram = !partiallist.instagram
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,instagram:!p.instagram}))
             },
             selected: listchannels.instagram
         },
         {
             icon: <MessageIcon color="inherit" />,
             label: 'Messenger',
+            key: 'messenger',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.messenger = !partiallist.messenger
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,messenger:!p.messenger}))
             },
             selected: listchannels.messenger
         },
         {
             icon: <WhatsAppIcon color="inherit" />,
             label: 'Whatsapp',
+            key: 'whatsapp',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.whatsapp = !partiallist.whatsapp
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,whatsapp:!p.whatsapp}))
             },
             selected: listchannels.whatsapp
         },
         {
             icon: <TelegramIcon color="inherit" />,
             label: 'Telegram',
+            key: 'telegram',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.telegram = !partiallist.telegram
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,telegram:!p.telegram}))
             },
             selected: listchannels.telegram
         },
         {
             icon: <TwitterIcon color="inherit" />,
             label: 'Twitter',
+            key: 'twitter',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.twitter = !partiallist.twitter
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,twitter:!p.twitter}))
             },
             selected: listchannels.twitter
         },
         {
             icon: <TwitterIcon color="inherit" />,
             label: 'Twitter DM',
+            key: 'twitterDM',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.twitterDM = !partiallist.twitterDM
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,twitterDM:!p.twitterDM}))
             },
             selected: listchannels.twitterDM
         },
@@ -225,20 +213,18 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
         {
             icon: <FacebookIcon color="inherit" />,
             label: 'Chat Web',
+            key: 'chatWeb',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.chatWeb = !partiallist.chatWeb
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,chatWeb:!p.chatWeb}))
             },
             selected: listchannels.chatWeb
         },
         {
             icon: <EmailIcon color="inherit" />,
             label: 'Email',
+            key: 'email',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.email = !partiallist.email
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,email:!p.email}))
             },
             selected: listchannels.email
             
@@ -246,20 +232,18 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
         {
             icon: <PhoneIcon color="inherit" />,
             label: 'Phone',
+            key: 'phone',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.phone = !partiallist.phone
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,phone:!p.phone}))
             },
             selected: listchannels.phone
         },
         {
             icon: <SmsIcon color="inherit" />,
             label: 'Sms',
+            key: 'sms',
             onClick: () => {
-                let partiallist = listchannels;
-                partiallist.sms = !partiallist.sms
-                setlistchannels(partiallist)
+                setlistchannels(p=>({...p,sms:!p.sms}))
             },
             selected: listchannels.sms
         },
@@ -274,17 +258,15 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
     }
     const handleClickShowPassword = () => setShowPassword(!showPassword);
     const handleMouseDownPassword = (event: any) => event.preventDefault();
-    const Option: FC<{ option: ChannelOption }> = ({ option }) => {
+    const Option: FC<{ option: ChannelOption, selected: Boolean }> = ({ option,selected }) => {
         const [color, setColor] = useState('#989898');
-
         return (
             <Paper
                 className={
                     clsx({
-                        [classes.optionContainerSelected]: option.selected,
-                        [classes.optionContainer]: !option.selected,
+                        [classes.optionContainerSelected]: selected,
+                        [classes.optionContainer]: !selected,
                     })
-                    //option.selected?classes.optionContainerSelected:classes.optionContainer
                 }
                 elevation={0}
                 onClick={option.onClick}
@@ -341,7 +323,6 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
                         label={t(langKeys.email)}
                         className="col-12"
                         variant="outlined"
-                        error={error.email}
                     />
                     <TextField
                         variant="outlined"
@@ -506,12 +487,13 @@ export const FirstStep: FC<{setStep:(param:any)=>void}> = ({setStep}) => {
                             <Option
                                 key={`social_media_option_${i}`}
                                 option={e}
+                                selected={listchannels[e.key]} 
                             />
                         )}
                     </div>
                     <Typography className={classes.subtitle}>{t(langKeys.businesschannel)}</Typography>
                     <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                        {businessChannelOptions.map((e, i) => <Option key={`business_channel_option_${i}`} option={e} />)}
+                        {businessChannelOptions.map((e, i) => <Option key={`business_channel_option_${i}`} option={e} selected={listchannels[e.key]} />)}
                     </div>
                 </div>
                 <div style={{ width: "100%" }}>
