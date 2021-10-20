7535c855b9dda925542a2a5c5b0de93164ff1e06
Diego Del Castillo <miguel.delgado@vcaperu.com>
2021-10-19 19:54:10 -0500

    Añadido canal Instagram DM para la creación de canales y subscripción

---
+++ src/pages/signup/ChannelAddInstagramDM.tsx
@@ -0,0 +1,257 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FC, useEffect, useState } from "react";
+import { makeStyles, Breadcrumbs, Button, Box, FormControlLabel, FormGroup } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import { showBackdrop, showSnackbar } from 'store/popus/actions';
+import { Facebook as FacebookIcon} from "@material-ui/icons";
+import { langKeys } from "lang/keys";
+import { useTranslation } from "react-i18next";
+import { FieldEdit, FieldSelect, ColorInput, IOSSwitch } from "components";
+import { useHistory } from "react-router";
+import { InstagramIcon} from "icons";
+import paths from "common/constants/paths";
+import FacebookLogin from 'react-facebook-login';
+import { useSelector } from "hooks";
+import { useDispatch } from "react-redux";
+import { getChannelsListSub } from "store/channel/actions";
+
+const useChannelAddStyles = makeStyles(theme => ({
+    button: {
+        padding: 12,
+        fontWeight: 500,
+        fontSize: '14px',
+        textTransform: 'initial',
+        width: "180px"
+    },
+}));
+
+export const ChannelAddInstagramDM: FC<{setrequestchannels:(param:any)=>void,setlistchannels:(param:any)=>void}> = ({setrequestchannels,setlistchannels}) => {
+    const [viewSelected, setViewSelected] = useState("view1");
+    const [waitSave, setWaitSave] = useState(false);
+    const [setins, setsetins] = useState(false);
+    const [nextbutton, setNextbutton] = useState(true);
+    const [channelreg, setChannelreg] = useState(true);
+    const mainResult = useSelector(state => state.channel.channelList)
+    const executeResult = useSelector(state => state.channel.successinsert)
+    const history = useHistory();
+    const [coloricon, setcoloricon] = useState("#F56040");
+    const [enable, setenable] = useState(false);
+    const dispatch = useDispatch();
+    const { t } = useTranslation();
+    const classes = useChannelAddStyles();
+    const [fields, setFields] = useState({
+        "method": "UFN_COMMUNICATIONCHANNEL_INS",
+        "parameters": {
+            "id": 0,
+            "description": "",
+            "type": "",
+            "communicationchannelsite": "",
+            "communicationchannelowner": "",
+            "chatflowenabled": false,
+            "integrationid": "",
+            "color": "",
+            "icons": "",
+            "other": "",
+            "form": "",
+            "apikey": "",
+            "coloricon": "#F56040",
+        },
+        "type": "INSTAMESSENGER",
+        "service": {
+            "accesstoken": "",
+            "siteid": "",
+            "appid": "1924971937716955"
+        }
+    })
+
+    async function finishreg() {
+        setrequestchannels((p:any)=>([...p,fields]))
+        setlistchannels((p:any)=>({...p,instagram:false}))
+    }
+    useEffect(() => {
+        if (waitSave && setins) {
+            if (mainResult.loading && executeResult) {
+                setsetins(false)
+                dispatch(showSnackbar({ show: true, success: true, message: t(langKeys.successful_register) }))
+                dispatch(showBackdrop(false));
+                setWaitSave(false);
+                history.push(paths.CHANNELS)
+            } else if (!executeResult) {
+                const errormessage = t(mainResult.code || "error_unexpected_error", { module: t(langKeys.property).toLocaleLowerCase() })
+                dispatch(showSnackbar({ show: true, success: false, message: errormessage }))
+                dispatch(showBackdrop(false));
+                setWaitSave(false);
+            }
+        }
+    }, [executeResult,waitSave])
+    useEffect(() => {
+        if (waitSave) {
+            dispatch(showBackdrop(false));
+            setWaitSave(false);
+        }
+    }, [mainResult])
+
+    const processFacebookCallback = async (r: any) => {
+        if (r.status !== "unknown" && !r.error) {
+            dispatch(getChannelsListSub(r.accessToken))
+            setViewSelected("view2")
+            dispatch(showBackdrop(true));
+            setWaitSave(true);
+        }
+    }
+    function setValueField(value: any) {
+        setNextbutton(value==null)
+        let partialf = fields;
+        partialf.parameters.communicationchannelsite = value?.id||""
+        partialf.parameters.communicationchannelowner = value?.name||""
+        partialf.service.siteid = value?.id||""
+        partialf.service.accesstoken = value?.access_token||""
+
+        setFields(partialf)
+    }
+    function setnameField(value: any) {
+        setChannelreg(value === "")
+        let partialf = fields;
+        partialf.parameters.description = value
+        setFields(partialf)
+    }
+    function setvalField(value: any) {
+        let partialf = fields;
+        partialf.parameters.chatflowenabled = value
+        setFields(partialf)
+    }
+    if(viewSelected==="view1"){
+        return (
+            <div style={{ width: '100%' }}>
+                <div>
+                    <div style={{ textAlign: "center", fontWeight: "bold", fontSize: "2em", color: "#7721ad", padding: "20px" }}>{t(langKeys.connectinsta)}</div>
+                    <div style={{ textAlign: "center", fontWeight: "bold", fontSize: "1.1em", padding: "20px" }}>{t(langKeys.connectinsta2)}</div>
+                    <div style={{ textAlign: "center", padding: "20px", color: "#969ea5" }}>{t(langKeys.connectinsta3)}</div>
+    
+                        <FacebookLogin
+                            appId="1924971937716955"
+                            autoLoad={false}
+                            buttonStyle={{ marginLeft: "calc(50% - 135px)", marginTop: "30px", marginBottom: "20px", backgroundColor: "#7721AD", textTransform: "none" }}
+                            fields="name,email,picture"
+                            scope="pages_show_list,instagram_basic,instagram_manage_comments,instagram_manage_insights,instagram_content_publish,pages_manage_metadata,public_profile"
+                            callback={processFacebookCallback}
+                            textButton={t(langKeys.linkinstagrampage)}
+                            onClick={(e: any) => {
+                                e.view.window.FB.init({
+                                    appId: '1924971937716955',
+                                    cookie: true,
+                                    xfbml: true,
+                                    version: 'v8.0'
+                                });
+                            }}
+                            icon={<FacebookIcon style={{ color: 'white', marginRight: '8px' }} />}
+                        />
+    
+                    <div style={{ textAlign: "center", paddingBottom: "80px", color: "#969ea5", fontStyle: "italic" }}>{t(langKeys.connectinsta4)}</div>
+    
+                </div>
+            </div>
+        )
+    }else if(viewSelected==="view2"){
+        return (
+            <div style={{ width: '100%' }}>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link color="textSecondary" key={"mainview"} href="/" onClick={(e) => { e.preventDefault(); setViewSelected("view1") }}>
+                        {"<< Previous"}
+                    </Link>
+                </Breadcrumbs>
+                <div>
+                    <div style={{ textAlign: "center", fontWeight: "bold", fontSize: "2em", color: "#7721ad", padding: "20px" }}>{t(langKeys.connectinsta)}</div>
+                    <div className="row-zyx">
+                        <div className="col-3"></div>
+                        <FieldSelect
+                            onChange={(value) => setValueField(value)}
+                            label={t(langKeys.selectpagelink)}
+                            className="col-6"
+                            data={mainResult.data}
+                            optionDesc="name"
+                            optionValue="id"
+                        />
+                    </div>
+
+                    <div style={{ paddingLeft: "80%" }}>
+                        <Button
+                            onClick={() => { setViewSelected("viewfinishreg") }}
+                            className={classes.button}
+                            variant="contained"
+                            color="primary"
+                            disabled={nextbutton}
+                        >{t(langKeys.next)}
+                        </Button>
+
+                    </div>
+
+                </div>
+            </div>
+        )
+    }else{
+        return (
+            <div style={{ width: '100%' }}>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link color="textSecondary" key={"mainview"} href="/" onClick={(e) => { e.preventDefault(); setViewSelected("view2") }}>
+                        {"<< Previous"}
+                    </Link>
+                </Breadcrumbs>
+                <div>
+                    <div style={{ textAlign: "center", fontWeight: "bold", fontSize: "2em", color: "#7721ad", padding: "20px", marginLeft: "auto", marginRight: "auto", maxWidth: "800px" }}>{t(langKeys.commchannelfinishreg)}</div>
+
+                    <div className="row-zyx">
+                        <div className="col-3"></div>
+                        <FieldEdit
+                            onChange={(value) => setnameField(value)}
+                            label={t(langKeys.givechannelname)}
+                            className="col-6"
+                        />
+                    </div>
+                    <div className="row-zyx">
+                        <div className="col-3"></div>
+                        <div className="col-6">
+                            <Box fontWeight={500} lineHeight="18px" fontSize={14} mb={1} color="textPrimary">
+                            {t(langKeys.givechannelcolor)}
+                            </Box>
+                            <div style={{display:"flex",justifyContent:"space-around", alignItems: "center"}}>
+                                <InstagramIcon style={{fill: `${coloricon}`, width: "100px" }}/>
+                                <ColorInput
+                                    hex={fields.parameters.coloricon}
+                                    onChange={e => {
+                                        setFields(prev => ({
+                                            ...prev,
+                                            parameters: { ...prev.parameters, coloricon: e.hex, color: e.hex },
+                                        }));
+                                        setcoloricon(e.hex)
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row-zyx">
+                        <div className="col-3"></div>
+                        <div className="col-6" style={{ paddingBottom: '3px' }}>
+                            <Box fontWeight={500} lineHeight="18px" fontSize={14} mb={2} color="textPrimary">{t(langKeys.enablechatflow)}</Box>
+                            <FormGroup>
+                                <FormControlLabel control={<IOSSwitch onChange={(e) => {setvalField(e.target.checked);setenable(e.target.checked)}}/>} label={enable?t(langKeys.enable):t(langKeys.disabled)} />
+                            </FormGroup>
+                        </div>
+                    </div>
+                    <div style={{ paddingLeft: "80%" }}>
+                        <Button
+                            onClick={() => { finishreg() }}
+                            className={classes.button}
+                            disabled={channelreg}
+                            variant="contained"
+                            color="primary"
+                        >{t(langKeys.finishreg)}
+                        </Button>
+
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
