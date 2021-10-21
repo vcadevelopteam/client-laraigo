e70d8fe872e5aea839643cac82c6d9744831f9ac
Pablo Rojas <projasb25@gmail.com>
2021-10-21 10:39:31 -0500

    crm

--- src/pages/crm/CRM.tsx
+++ src/pages/crm/CRM.tsx
@@ -1,11 +1,149 @@
-import React, { FC } from 'react';
+import { uuidv4 } from "common/helpers";
+import React, { FC, useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const itemsFromBackend = [
+    { id: uuidv4(), content: "First task" },
+    { id: uuidv4(), content: "Second task" },
+    { id: uuidv4(), content: "Third task" },
+    { id: uuidv4(), content: "Fourth task" },
+    { id: uuidv4(), content: "Fifth task" }
+  ];
+
+  const columnsFromBackend = {
+    [uuidv4()]: {
+      name: "Requested",
+      items: itemsFromBackend
+    },
+    [uuidv4()]: {
+      name: "To do",
+      items: []
+    },
+    [uuidv4()]: {
+      name: "In Progress",
+      items: []
+    },
+    [uuidv4()]: {
+      name: "Done",
+      items: []
+    }
+  };
+
+  const onDragEnd = (result:any, columns:any, setColumns:any) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+  
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+  };
 
 const CRM: FC = () => {
+    const [columns, setColumns] = useState(columnsFromBackend);
     return (
-        <div>
-            CRM
+        <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+          <DragDropContext
+            onDragEnd={result => onDragEnd(result, columns, setColumns)}
+          >
+            {Object.entries(columns).map(([columnId, column], index) => {
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center"
+                  }}
+                  key={columnId}
+                >
+                  <h2>{column.name}</h2>
+                  <div style={{ margin: 8 }}>
+                    <Droppable droppableId={columnId} key={columnId}>
+                      {(provided, snapshot) => {
+                        return (
+                          <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            style={{
+                              background: snapshot.isDraggingOver
+                                ? "lightblue"
+                                : "lightgrey",
+                              padding: 4,
+                              width: 250,
+                              minHeight: 500
+                            }}
+                          >
+                            {column.items.map((item, index) => {
+                              return (
+                                <Draggable
+                                  key={item.id}
+                                  draggableId={item.id}
+                                  index={index}
+                                >
+                                  {(provided, snapshot) => {
+                                    return (
+                                      <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={{
+                                          userSelect: "none",
+                                          padding: 16,
+                                          margin: "0 0 8px 0",
+                                          minHeight: "50px",
+                                          backgroundColor: snapshot.isDragging
+                                            ? "#263B4A"
+                                            : "#456C86",
+                                          color: "white",
+                                          ...provided.draggableProps.style
+                                        }}
+                                      >
+                                        {item.content}
+                                      </div>
+                                    );
+                                  }}
+                                </Draggable>
+                              );
+                            })}
+                            {provided.placeholder}
+                          </div>
+                        );
+                      }}
+                    </Droppable>
+                  </div>
+                </div>
+              );
+            })}
+          </DragDropContext>
         </div>
-    );
+      );
 };
 
 export default CRM;
