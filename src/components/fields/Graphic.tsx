2bfe37239d746154236405b8be00c9cfee08a0ac
nanoxxi93 <hernando.arima@vcaperu.com>
2022-01-24 17:31:30 -0500

    Fix Graphic color random

--- src/components/fields/Graphic.tsx
+++ src/components/fields/Graphic.tsx
@@ -87,7 +87,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-var randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+var randomColor = () => Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 
 interface IGraphic {
     graphicType: string;
