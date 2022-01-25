294f239965a0335e96dc32ac38e1d2cf5b60a2f5
Diego Del Castillo <miguel.delgado@vcaperu.com>
2022-01-25 16:40:39 -0500

    Limpieza en las funciones de facturación

--- src/lang/keys.ts
+++ src/lang/keys.ts
@@ -2765,6 +2765,7 @@ export const langKeys = {
     invoicenotfound: 'invoicenotfound',
     saveasdraft: 'saveasdraft',
     deleteinvoicesuccess: 'deleteinvoicesuccess',
+    field_required_shorter: 'field_required_shorter',
 };
 
 export type LangKeys = typeof langKeys;
