a63f02cb60401ec957f9a452ee24d7b09bc4eea1
Carlos Farro <carlos.farro@vcaperu.com>
2022-03-25 16:48:01 -0500

    fix

--- src/components/fields/Schedule.tsx
+++ src/components/fields/Schedule.tsx
@@ -3,6 +3,7 @@ import { ArrowDropDown } from "@material-ui/icons";
 import Close from "@material-ui/icons/Close";
 import React, { FC, useCallback, useEffect, useState } from "react";
 import { ChromePicker, ColorChangeHandler, ColorResult } from "react-color";
+import clsx from 'clsx';
 
 const dayNames = [
     'sunday',
@@ -34,11 +35,15 @@ const useScheduleStyles = makeStyles(theme => ({
         borderRight: '1px solid #e0e0e0',
         borderBottom: '1px solid #e0e0e0',
         padding: 8,
-        
+
         '&:hover': {
             padding: 6,
             border: '2px solid #5593ff'
         }
+    },
+    isToday: {
+        fontWeight: 'bold',
+        backgroundColor: '#e1e1e1'
     },
     dow: {
         fontWeight: 'bold'
@@ -53,13 +58,13 @@ const useScheduleStyles = makeStyles(theme => ({
 }));
 
 
-const Schedule: FC<ScheduleInputProps> = ({  }) => {
+const Schedule: FC<ScheduleInputProps> = ({ }) => {
     const classes = useScheduleStyles();
     const [daysToShow, setDaysToShow] = useState<DayProp[]>([]);
-
+    console.log(daysToShow)
     useEffect(() => {
         const year = new Date().getFullYear();
-        const month = new Date().getMonth() + 1;
+        const month = new Date().getMonth();
         const countDays = new Date(year, month, 0).getDate();
         const dayLastDay = new Date(year, month, 0).getDay();
         const dayPreviewMonth = new Date(year, month - 1, 0).getDay();
@@ -75,30 +80,35 @@ const Schedule: FC<ScheduleInputProps> = ({  }) => {
         })
 
         const daysPreviewMonth = Array.from(Array(dayPreviewMonth).keys()).map(x => {
-            const date = new Date(year, month - 1, - (x + 1));
+            const date = new Date(year, month, - x);
             return {
                 date: date.toISOString().slice(0, 10),
                 dow: date.getDay(),
                 dom: date.getDate()
             }
         })
-        
+
         const daysNextMonth = Array.from(Array(7 - dayLastDay).keys()).map(x => {
-            const date = new Date(year, month + 1, x + 1);
+            const date = new Date(year, month, x + 1);
             return {
                 date: date.toISOString().slice(0, 10),
                 dow: date.getDay(),
                 dom: date.getDate()
             }
         })
-        
+
         setDaysToShow([...daysPreviewMonth, ...daysMonth, ...daysNextMonth]);
     }, [])
 
     return (
         <div className={classes.wrapper}>
             {daysToShow.map((day, index) => (
-                <div className={classes.boxDay} key={index}>
+                <div
+                    className={clsx(classes.boxDay, {
+                        [classes.isToday]: day.isToday
+                    })}
+                    key={index}
+                >
                     <div className={classes.dow}>{day.dom}</div>
                 </div>
             ))}
