5d3b44040cb706bc96dcb22ba4bc7604d45ecf85
Jose Vara <desarrollo.vca@vcaperu.com>
2022-03-30 18:57:16 -0500

    f

--- src/pages/Invoice.tsx
+++ src/pages/Invoice.tsx
@@ -2968,7 +2968,7 @@ const Billing: React.FC <{ dataPlan: any}> = ({ dataPlan }) => {
         const dataMonths =[{ val: "1" }, { val: "2" }, { val: "3" }, { val: "4" }, { val: "5" }, { val: "6" }, { val: "7" }, { val: "8" }, { val: "9" }, { val: "10" }, { val: "11" }, { val: "12" }];
         const dataCurrency = [{ value: "PEN", description: "PEN" }, { value: "USD", description: "USD" }]
         const receiverdoctype = [{ value: 0, description: "NO DOMICILIADO" }, { value: 1, description: "DNI" }, { value: 4, description: "CARNE EXT." }, { value: 6, description: "RUC" }, { value: 7, description: "PASAPORTE" }]
-        const invoicetype = [{ value: "01", description: "FACTURA" }, { value: "03", description: "BOLETA" }]
+        const invoicetype = [{ value: "1", description: "FACTURA" }, { value: "3", description: "BOLETA" }]
         const indexCreditType = multiResult.data.findIndex((x: MultiData) => x.key === ('UFN_DOMAIN_LST_VALORES'));
         let credittypelist = multiResult.data[indexCreditType] && multiResult.data[indexCreditType].success ? multiResult.data[indexCreditType].data : []
         let corplist = multiResult.data[indexCorp] && multiResult.data[indexCorp].success ? multiResult.data[indexCorp].data : [] 
@@ -3037,13 +3037,14 @@ const Billing: React.FC <{ dataPlan: any}> = ({ dataPlan }) => {
                 && !['', null, undefined].includes(d.paymentstatus)
                 && !['', null, undefined].includes(d.totalamount)
                 && !['', null, undefined].includes(d.exchangerate)
-                && Object.keys(invoicetypeList.reduce((a,d) => ({...a, [d.value]: d.description}), {})).includes('' + d.invoicetype)
+                && Object.keys(invoicetypeList.reduce((a,d) => ({...a, [d.value]: d.description}), {})).includes('0' + d.invoicetype)
                 && Object.keys(receiverdoctypeList.reduce((a,d) => ({...a, [d.value]: d.description}), {})).includes('' + d.receiverdoctype)
                 && Object.keys(corplist.reduce((a,d) => ({...a, [d.corpid]: d.description}), {})).includes('' + d.corpid)
                 && Object.keys(dataYears.reduce((a,d) => ({...a, [d.desc]: d.desc}), {})).includes('' + d.year)
                 && Object.keys(dataMonths.reduce((a,d) => ({...a, [d.val]: d.val}), {})).includes('' + d.month)
                 && Object.keys(dataCurrency.reduce((a,d) => ({...a, [d.value]: d.description}), {})).includes('' + d.currency)
             );
+            console.log(data)
             debugger
             if (data.length > 0) {
                 dispatch(showBackdrop(true));
