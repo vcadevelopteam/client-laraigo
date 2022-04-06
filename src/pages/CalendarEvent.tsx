8c6eb3b93189827683f5d0f69bfaeb57b649564a
Carlos Farro <carlos.farro@vcaperu.com>
2022-04-06 16:17:39 -0500

    fix

--- src/pages/CalendarEvent.tsx
+++ src/pages/CalendarEvent.tsx
@@ -5,7 +5,7 @@ import { Button, Fab, IconButton, makeStyles, Typography } from "@material-ui/co
 import { useParams } from 'react-router';
 import { FieldEdit, CalendarZyx, FieldEditMulti } from "components";
 import { getCollEventBooking } from 'store/main/actions';
-import { getEventByCode, validateCalendaryBooking, dayNames, calculateDateFromMonth, insBookingCalendar } from 'common/helpers';
+import { getEventByCode, validateCalendaryBooking, dayNames, calculateDateFromMonth, insBookingCalendar, getPersonFromBooking } from 'common/helpers';
 import { Dictionary } from '@types';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import ScheduleIcon from '@material-ui/icons/Schedule';
@@ -179,10 +179,12 @@ const TimeDate: FC<{ time: ITime, isSelected: boolean, setTimeSelected: (p: any)
     )
 }
 
-const FormToSend: FC<{ event: Dictionary, handlerOnSubmit: (p: any) => void, disabledSubmit: boolean }> = ({ event, handlerOnSubmit, disabledSubmit }) => {
+const FormToSend: FC<{ event: Dictionary, handlerOnSubmit: (p: any) => void, disabledSubmit: boolean, parameters: Dictionary }> = ({ event, handlerOnSubmit, disabledSubmit, parameters }) => {
     const { t } = useTranslation();
     const classes = useStyles();
 
+    console.log(parameters)
+    
     const { register, handleSubmit, setValue, getValues, formState: { errors } } = useForm({
         defaultValues: {
             name: '',
@@ -298,20 +300,21 @@ export const CalendarEvent: FC = () => {
     const onChangeMonth = (month: number, year: number) => {
         setDateCurrent({ month, year });
     }
-    console.log(ticket)
 
     useEffect(() => {
-        dispatch(getCollEventBooking(getEventByCode(orgid, eventcode)))
+        let personid = 0;
         const query = new URLSearchParams(location.search);
         const posibleConversationID = query.get('cid');
         const posiblePersonID = query.get('pid');
 
         if (posibleConversationID && Number.isInteger(Number(posibleConversationID)) && posiblePersonID && Number.isInteger(Number(posiblePersonID))) {
+            personid = Number(posiblePersonID);
             setTicket({
                 conversationid: Number(posibleConversationID),
                 personid: Number(posiblePersonID)
             })
         }
+        dispatch(getCollEventBooking(getEventByCode(orgid, eventcode, personid)))
     }, [])
 
     const triggerCalculateDate = () => {
@@ -481,6 +484,11 @@ export const CalendarEvent: FC = () => {
                                 event={event!!}
                                 handlerOnSubmit={handlerOnSubmit}
                                 disabledSubmit={resMain.loading && !!event}
+                                parameters={{
+                                    corpid: event?.corpid,
+                                    orgid: event?.orgid,
+                                    personid: ticket?.personid,
+                                }}
                             />
                         </div>
                     )}
