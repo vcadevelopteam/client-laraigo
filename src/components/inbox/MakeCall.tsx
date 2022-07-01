806855e8f8c2c9462cdee3d7bb942c6203117ab3
Carlos Farro <carlos.farro@vcaperu.com>
2022-07-01 10:01:59 -0500

    send voximplant recording to scenario

--- src/components/inbox/MakeCall.tsx
+++ src/components/inbox/MakeCall.tsx
@@ -225,7 +225,7 @@ const MakeCall: React.FC = () => {
     const classes = useStyles();
     const { t } = useTranslation();
     const dispatch = useDispatch();
-    
+
     const personData = useSelector(state => state.inbox.person);
 
     const [numberVox, setNumberVox] = useState("");
@@ -249,7 +249,7 @@ const MakeCall: React.FC = () => {
         console.log(resExecute)
         if (!resExecute.loading && !resExecute.error) {
             if (resExecute.key === "UFN_CONVERSATION_OUTBOUND_INS") {
-                const { v_conversationid, v_ticketnum, v_personid, v_firstconversationdate, v_personname } = resExecute.data[0]
+                const { v_conversationid, v_ticketnum, v_personid, v_firstconversationdate, v_personname, v_voximplantrecording } = resExecute.data[0]
                 const data: ITicket = {
                     conversationid: parseInt(v_conversationid),
                     ticketnum: v_ticketnum,
@@ -270,7 +270,8 @@ const MakeCall: React.FC = () => {
                     lastreplyuser: "",
                 }
                 dispatch(setModalCall(false));
-                const identifier = `${corpid}-${orgid}-${ccidvoxi}-${resExecute.data[0].v_conversationid}-${resExecute.data[0].v_personid}.${sitevoxi}.${userid}`
+                const identifier = `${corpid}-${orgid}-${ccidvoxi}-${resExecute.data[0].v_conversationid}-${resExecute.data[0].v_personid}.${sitevoxi}.${userid}.${v_voximplantrecording}`;
+
                 dispatch(makeCall({ number: numberVox, site: identifier || "", data }));
             }
         } else if (!resExecute.loading && resExecute.error && resExecute.key === "UFN_CONVERSATION_OUTBOUND_INS") {
@@ -334,7 +335,7 @@ const MakeCall: React.FC = () => {
     React.useEffect(() => {
         if (showcall) {
             setwaiting2(false)
-            setNumberVox(personData?.data?.phone||"")
+            setNumberVox(personData?.data?.phone || "")
         } else {
             setPageSelected(1)
         }
@@ -365,7 +366,7 @@ const MakeCall: React.FC = () => {
                         variant="fullWidth"
                         style={{ borderBottom: '1px solid white', backgroundColor: '#7721ad' }}
                         textColor="primary"
-                        onChange={(_, value) => {setPageSelected(value); setGlobalFilter("")}}
+                        onChange={(_, value) => { setPageSelected(value); setGlobalFilter("") }}
                     >
                         <AntTab label={<ContactPhoneIcon style={{ color: pageSelected === 0 ? "gold" : "white" }} />} />
                         <AntTab label={<DialpadIcon style={{ color: pageSelected === 1 ? "gold" : "white" }} />} />
@@ -389,7 +390,7 @@ const MakeCall: React.FC = () => {
                                 if (filter === "") {
                                     return false;
                                 }
-                                if(filter.toLowerCase() === "sin nombre"){
+                                if (filter.toLowerCase() === "sin nombre") {
                                     return (x.personname?.trim() === x.phone?.trim())
                                 }
                                 return (x.personname?.toLowerCase()?.includes(filter.toLowerCase()) || x.phone?.includes(filter))
@@ -426,7 +427,7 @@ const MakeCall: React.FC = () => {
                                     value={numberVox}
                                     disabled={resExecute.loading || statusCall !== "DISCONNECTED"}
                                     style={{ marginRight: "auto", marginLeft: "auto", width: "400px", marginBottom: 25 }}
-                                    onInput={(e:any)=>{
+                                    onInput={(e: any) => {
                                         let val = e.target.value.replace(/[^0-9*#]/g, "")
                                         e.target.value = String(val)
                                     }}
@@ -587,3 +588,4 @@ const formatDate = (strDate: string) => {
 
     return `${day}/${month}/${year}`;
 }
+
