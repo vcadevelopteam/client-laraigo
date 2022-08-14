6287931a62362e5d9d3dfd5cf875fe8447b3fc56
desarrollo.vca <desarrollo.vca@vcaperu.com>
2022-08-14 10:45:05 -0500

    RLACL027. Agregar campo Grupo

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1093,7 +1093,7 @@ const Users: FC = () => {
             { 'true': 'true', 'false': 'false' },
             domains.value?.roles?.reduce((a, d) => ({ ...a, [d.roleid]: d.roldesc }), {}),
             dataChannelsTemp.reduce((a, d) => ({ ...a, [d.communicationchannelid]: d.description }), {}),
-            domains.value?.groups?.reduce((a, d) => ({ ...a, [d.domainvalue]: d.domaindesc }), {}),
+            domains.value?.usergroup?.reduce((a, d) => ({ ...a, [d.domainvalue]: d.domaindesc }), {}),
         ];
         const header = [
             'firstname',
@@ -1112,7 +1112,7 @@ const Users: FC = () => {
             'pwdchangefirstlogin',
             'role',
             'channels',
-            'group'
+            'groups'
         ];
         exportExcel(`${t(langKeys.template)} ${t(langKeys.import)}`, templateMaker(data, header));
     }
@@ -1250,9 +1250,9 @@ const Users: FC = () => {
                     && (f.status === undefined || Object.keys(domains.value?.userstatus?.reduce((a: any, d) => ({ ...a, [d.domainvalue]: d.domainvalue }), {})).includes('' + f.status))
                     && (f.pwdchangefirstlogin === undefined || ["true", "false"].includes('' + f.pwdchangefirstlogin))
                     && (f.role === undefined || Object.keys(domains.value?.roles?.reduce((a: any, d) => ({ ...a, [d.roleid]: `${d.roleid}` }), {})).includes('' + f.role))
-                    && (f.group === undefined || Object.keys(domains.value?.groups?.reduce((a: any, d) => ({ ...a, [d.domainid]: `${d.domainid}` }), {})).includes('' + f.group))
             });
 
+            debugger
             const messageerrors = datainit.filter((f: any) => {
                 return !(f.company === undefined || Object.keys(domains.value?.company?.reduce((a: any, d) => ({ ...a, [d.domainvalue]: d.domainvalue }), {})).includes('' + f.company))
                     || !(f.doctype === undefined || Object.keys(domains.value?.docTypes?.reduce((a: any, d) => ({ ...a, [d.domainvalue]: d.domainvalue }), {})).includes('' + f.doctype))
@@ -1261,7 +1261,6 @@ const Users: FC = () => {
                     || !(f.status === undefined || Object.keys(domains.value?.userstatus?.reduce((a: any, d) => ({ ...a, [d.domainvalue]: d.domainvalue }), {})).includes('' + f.status))
                     || !(f.pwdchangefirstlogin === undefined || ["true", "false"].includes('' + f.pwdchangefirstlogin))
                     || !(f.role === undefined || Object.keys(domains.value?.roles?.reduce((a: any, d) => ({ ...a, [d.roleid]: `${d.roleid}` }), {})).includes('' + f.role))
-                    || !(f.group === undefined || Object.keys(domains.value?.groups?.reduce((a: any, d) => ({ ...a, [d.domainid]: `${d.domainid}` }), {})).includes('' + f.group))
             }).reduce((acc, x) => acc + t(langKeys.error_estructure_user, { email: x.email }) + `\n`, '');
 
             setMessageError(messageerrors)
@@ -1298,7 +1297,7 @@ const Users: FC = () => {
                                 orgid: user?.orgid,
                                 bydefault: true,
                                 labels: "",
-                                groups: d.group,
+                                groups: d.groups || "",
                                 channels: d.channels || "",
                                 status: d.status,
                                 type: "NINGUNO",
