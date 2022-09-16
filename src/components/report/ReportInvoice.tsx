fc143bdfbeadea664c84e81b73346df08f3b7602
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2022-09-16 10:44:46 -0500

    RLA222-1. Actualizar el listado de facturas si se realizo un cambio en comentarios

--- src/components/report/ReportInvoice.tsx
+++ src/components/report/ReportInvoice.tsx
@@ -337,6 +337,7 @@ const DetailReportInvoice: React.FC<DetailReportInvoiceProps> = ({ data: { row, 
 
     const onModalSuccess = () => {
         setOpenModal(false);
+        search();
         setViewSelected("view-2");
     }
 
@@ -387,6 +388,7 @@ const InvoiceCommentModal: FC<{ data: any, openModal: boolean, setOpenModal: (pa
     const [waitSave, setWaitSave] = useState(false);
     const [waitLoad, setWaitLoad] = useState(false);
     const [contentValidation, setContentValidation] = useState('');
+    const [reloadExit, setReloadExit] = useState(false);
 
     const [fields, setFields] = useState({
         "corpid": data?.corpid,
@@ -416,6 +418,7 @@ const InvoiceCommentModal: FC<{ data: any, openModal: boolean, setOpenModal: (pa
         if (openModal && data) {
             setDataInvoiceComment([]);
             setContentValidation('');
+            setReloadExit(false);
 
             let partialFields = fields;
             partialFields.corpid = data?.corpid;
@@ -461,6 +464,8 @@ const InvoiceCommentModal: FC<{ data: any, openModal: boolean, setOpenModal: (pa
                 setFields(partialFields);
 
                 fetchData();
+
+                setReloadExit(true);
             } else if (executeResult.error) {
                 dispatch(showSnackbar({ show: true, severity: "error", message: t(executeResult.code || "error_unexpected_error", { module: t(langKeys.organization_plural).toLocaleLowerCase() }) }));
                 dispatch(showBackdrop(false));
@@ -526,7 +531,7 @@ const InvoiceCommentModal: FC<{ data: any, openModal: boolean, setOpenModal: (pa
             open={openModal}
             title={t(langKeys.invoicecomments)}
             buttonText1={t(langKeys.close)}
-            handleClickButton1={() => setOpenModal(false)}
+            handleClickButton1={() => { setOpenModal(false); if (reloadExit) { onTrigger(); } }}
         >
             <div style={{ overflowY: 'auto' }}>
                 {dataInvoiceComment.map((item, index) => (
