91d33752a04b14a05d2c663967d79d1aaec39cf3
desarrollo.vca <desarrollo.vca@vcaperu.com>
2022-10-18 16:56:30 -0500

    RLA249

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1042,7 +1042,7 @@ const Reports: FC = () => {
                                     component="img"
                                     height="140"
                                     className={classes.media}
-                                    image={'https://staticfileszyxme.s3.us-east.cloud-object-storage.appdomain.cloud/LARAIGO%20-%20ACME/2477ae23-02a0-401b-987c-677159d47c0b/iconosvcaperu_720.png'}
+                                    image={'https://staticfileszyxme.s3.us-east.cloud-object-storage.appdomain.cloud/CLARO/72fd542f-719c-4226-89f9-b29c6a46a4d7/reporteKPI.svg'}
                                     title={t(langKeys.kpimanager)}
                                 />
                                 <CardContent>
