2607ed9e48065ba31ab2818d87d2c77def95041d
Carlos Farro <carlos.farro@vcaperu.com>
2022-11-24 22:27:54 -0500

    RLA276. avance5

--- src/middlewares/voximplantMiddleware.ts
+++ src/middlewares/voximplantMiddleware.ts
@@ -163,7 +163,8 @@ const calVoximplantMiddleware: Middleware = ({ dispatch }) => (next: Dispatch) =
             payload: {
                 call,
                 type: "OUTBOUND",
-                number: payload.number
+                number: payload.number,
+                statusCall: "CONNECTING"
             }
         })
 
