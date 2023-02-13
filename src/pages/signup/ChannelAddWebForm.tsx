32bd5f8bff7a7ce910a073af44a53ca24fd1ba6b
desarrollo.vca <desarrollo.vca@vcaperu.com>
2023-02-13 17:03:07 -0500

    fix

--- src/pages/signup/ChannelAddWebForm.tsx
+++ src/pages/signup/ChannelAddWebForm.tsx
@@ -85,6 +85,8 @@ const TabPanelInterface: FC<{ form: UseFormReturn<IFormWebAdd> }> = ({ form }) =
     const { setValue, getValues } = form;
     const classes = useTabInterfacetyles();
     const { t } = useTranslation();
+    const [recaptcha, setrecaptcha] = useState(getValues('extra.recaptcha'));
+
 
     return (
         <Grid container direction="column">
@@ -192,9 +194,8 @@ const TabPanelInterface: FC<{ form: UseFormReturn<IFormWebAdd> }> = ({ form }) =
                         </Grid>
                         <Grid item xs={12} sm={9} md={9} lg={9} xl={9}>
                             <FormControlLabel
-                                value={getValues('extra.recaptcha')}
                                 control={<Checkbox 
-                                    onChange={(e) => setValue('extra.recaptcha', e.target.checked)}
+                                    onChange={(e) => {setrecaptcha(e.target.checked);setValue('extra.recaptcha', e.target.checked)}}
                                     color="primary" />}
                                 //onChange={(e) => {setValue('extra.recaptcha', e.target.value === "true")}}
                                 label={t(langKeys.recaptchaCheckbox)}
