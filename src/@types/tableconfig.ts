8f74d14dbade56ebcbc5734eebc975f3d032e0b8
Carlos Farro <fdcarlosd1@gmail.com>
2023-07-04 09:11:46 -0500

    new acceptTypeLoad on table-simple

--- src/@types/tableconfig.ts
+++ src/@types/tableconfig.ts
@@ -60,6 +60,7 @@ export interface TableConfig {
   onFilterChange?: (filter: ITablePaginatedFilter) => void;
   checkHistoryCenter?: boolean;
   helperText?: string;
+  acceptTypeLoad?: string;
   initialStateFilter?: {
     id: string;
     value: {
