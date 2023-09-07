2c49f7d552db956631c17dcac0ffa99285dfaa3f
Pablo Rojas <pablo.rojas@latinfintech.com>
2023-09-07 00:30:04 -0500

    new config alias to fix 360dialog package

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    
     plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
     server: {
         port: 3000,
         https: false,
     },
     build: {
-        outDir: 'build',
-        
+        outDir: "build",
     },
-    
+    resolve: {
+        alias: [
+            {
+                find: "360dialog-connect-button",
+                replacement: path.resolve(
+                    __dirname,
+                    "node_modules/360dialog-connect-button/dist/dialog-connect-button.esm.js"
+                ),
+            },
+        ],
+    },
 });
