7d9c30dc3465e13f27df54fa98634847ad31c096
JoseVara <jose.vara@vcaperu.com>
2023-10-04 11:01:49 -0500

    reservations dialog

--- src/pages/inventory/inventory/dialogs/ManageReservationsDialog.tsx
+++ src/pages/inventory/inventory/dialogs/ManageReservationsDialog.tsx
@@ -10,7 +10,7 @@ import { useEffect, useState } from "react";
 import ClearIcon from "@material-ui/icons/Clear";
 import { useTranslation } from "react-i18next";
 import SaveIcon from "@material-ui/icons/Save";
-import { getInventoryBooking, insProductAttribute } from "common/helpers";
+import { getInventoryBooking, insInventoryBooking, insProductAttribute } from "common/helpers";
 import { execute, getCollectionAux2, resetMainAux } from "store/main/actions";
 import { TemplateIcons } from 'components';
 import { useDispatch } from "react-redux";
@@ -34,39 +34,28 @@ const ManageReservationsDialog: React.FC<{
   row: any;
 }> = ({ openModal, setOpenModal, row }) => {
   const { t } = useTranslation();
-  const classes = useStyles();
   const dispatch = useDispatch();
   const [waitSave, setWaitSave] = useState(false);
   const executeRes = useSelector(state => state.main.execute);
   const [openModalAddReservation, setOpenModalAddReservation] = useState(false);
   const data = useSelector(state => state.main.mainAux2);
 
+
+  const fetchData = () => {
+    dispatch(getCollectionAux2(getInventoryBooking(row?.inventoryid)))
+  }
   useEffect(() => {
     if(openModal){
-      dispatch(getCollectionAux2(getInventoryBooking(row?.inventoryid)));
+      fetchData()
     }
   }, [openModal]);
-
-  const { register, handleSubmit:handleMainSubmit, setValue, getValues, reset} = useForm({
-    defaultValues: {
-        productattributeid: 0,
-        productid: 0,
-        attributeid: '',
-        value: '',
-        unitmeasureid: 0,
-        status: 'ACTIVO',
-        type: 'NINGUNO',
-        operation: "INSERT"
-    }
-  });
 
   useEffect(() => {
     if (waitSave) {
         if (!executeRes.loading && !executeRes.error) {
             dispatch(showSnackbar({ show: true, severity: "success", message: t(row ? langKeys.successful_edit : langKeys.successful_register) }))
             dispatch(showBackdrop(false));
-            reset()
-            setOpenModal(false);
+            fetchData()
         } else if (executeRes.error) {
             const errormessage = t(executeRes.code || "error_unexpected_error", { module: t(langKeys.domain).toLocaleLowerCase() })
             dispatch(showSnackbar({ show: true, severity: "error", message: errormessage }))
@@ -76,35 +65,18 @@ const ManageReservationsDialog: React.FC<{
     }
 }, [executeRes, waitSave])
 
-React.useEffect(() => {
-  register('unitmeasureid', { validate: (value) =>((value && value>0) ? true : t(langKeys.field_required) + "") });
-  register('attributeid', { validate: (value) =>((value && value.length>0) ? true : t(langKeys.field_required) + "") });
-  register('value', { validate: (value) =>((value && value.length>0) ? true : t(langKeys.field_required) + "") });
-}, [register]);
-  
-const submitData = handleMainSubmit((data) => {
+const handleDelete = (row: Dictionary) => {
   const callback = () => {
+      dispatch(execute(insInventoryBooking({ ...row, operation: 'DELETE', status: 'ELIMINADO' })));
       dispatch(showBackdrop(true));
-      //dispatch(execute(insProductAttribute(data)));
       setWaitSave(true);
   }
+
   dispatch(manageConfirmation({
       visible: true,
-      question: t(langKeys.confirmation_save),
+      question: t(langKeys.confirmation_delete),
       callback
   }))
-});
-
-const handleDelete = (data: Dictionary) => {
-  /*dispatch(execute(insProductAlternative({
-    productalternativeid: data.productalternativeid,
-    productid: data.productid,
-    productaltid: data.productid,
-    status: 'ELIMINADO',
-    type: 'NINGUNO',
-    operation: 'DELETE'
-  })))*/
-  setWaitSave(true);
 }
 
 const columns = React.useMemo(
@@ -190,7 +162,6 @@ function handleOpenAddReservationModal() {
             style={{ backgroundColor: "#FB5F5F" }}
             onClick={() => {
               setOpenModal(false);
-              reset()
             }}
           >
             {t(langKeys.back)}
@@ -201,6 +172,7 @@ function handleOpenAddReservationModal() {
         openModal={openModalAddReservation}
         setOpenModal={setOpenModalAddReservation}
         row={row}
+        fetchData={fetchData}
       />
     </>
   );
