7ac2d0505b0930229596735e38d82a2553c171c6
nirvaGarciaV <nirvana.garcia@vcaperu.com>
2023-11-17 23:48:20 -0500

    adding functionalities on google maps

--- src/components/fields/GoogleMapsPrueba.tsx
+++ src/components/fields/GoogleMapsPrueba.tsx
@@ -1,64 +1,100 @@
-import React from "react";
-import { GoogleMap, useLoadScript, Polygon } from "@react-google-maps/api";
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Polygon } from '@react-google-maps/api';
 
-interface MapContainerProps {
-    coordinates: {
-    points: {
-        lat: number;
-        lng: number;
-    }[];
+const GoogleMaps = () => {
+  const mapContainerStyle = {
+    height: '20rem',
+    width: '100%',
+  };
+
+  const center = {
+    lat: 37.7749,
+    lng: -122.4194,
+  };
+
+  const defaultPolygonCoords = [
+    { lat: 37.7749, lng: -122.4194 },
+    { lat: 37.7749, lng: -122.4198 },
+    { lat: 37.7752, lng: -122.4198 },
+    { lat: 37.7752, lng: -122.4194 },
+  ];
+
+  const [polygonCoords, setPolygonCoords] = useState(defaultPolygonCoords);
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [polygon, setPolygon] = useState<google.maps.Polygon | null>(null);
+
+  const updatePolygonCoords = () => {
+    if (polygon) {
+      const updatedCoords = polygon
+        .getPath()
+        .getArray()
+        .map((coord) => {
+          return { lat: coord.lat(), lng: coord.lng() };
+        });
+      console.log('Polygon Updated:', updatedCoords);
+      setPolygonCoords(updatedCoords);
+    }
+  };
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.defer = true;
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
     };
-    onMapClick: (e: { lat: number; lng: number }) => void;
-}
+  }, []);
 
-const mapStyles = {
-    height: "20rem",
-    width: "100%",
-};
-
-const GoogleMaps: React.FC<MapContainerProps> = ({ coordinates, onMapClick }) => {
-    const defaultCenter = {
-        lat: coordinates.points[0].lat,
-        lng: coordinates.points[0].lng,
-    };
-
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: "AIzaSyCBij6DbsB8SQC_RRKm3-X07RLmvQEnP9w",
-    });
-
-    if (loadError) return <div>Error loading Google Maps</div>;
-    if (!isLoaded) return <div>Loading Google Maps...</div>;
-
-    return (
+  return (
+    <div>      
+        <LoadScript googleMapsApiKey="AIzaSyCBij6DbsB8SQC_RRKm3-X07RLmvQEnP9w">
         <GoogleMap
-            mapContainerStyle={mapStyles}
-            zoom={15}
-            center={defaultCenter}
-            onClick={(e) => {
-            if (e.latLng) {
-                onMapClick({
-                lat: e.latLng.lat(),
-                lng: e.latLng.lng(),
-                });
-            }
-            }}
+          mapContainerStyle={mapContainerStyle}
+          center={center}
+          zoom={14}
+          onLoad={(map) => {
+            setMap(map);
+
+            const newPolygon = new window.google.maps.Polygon({
+              paths: defaultPolygonCoords,
+              editable: true,
+              draggable: true,
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#FF0000',
+              fillOpacity: 0.35,
+            });
+
+            newPolygon.setMap(map);
+            setPolygon(newPolygon);
+
+            window.google.maps.event.addListener(newPolygon.getPath(), 'set_at', updatePolygonCoords);
+            window.google.maps.event.addListener(newPolygon, 'dragend', updatePolygonCoords);
+          }}
         >
+          {polygon && (
             <Polygon
-            paths={coordinates.points}
-            options={{
-                fillColor: "#7721AD",
-                fillOpacity: 0.4,
-                strokeColor: "#7721AD",
-                strokeOpacity: 1,
+              paths={polygonCoords}
+              editable
+              draggable
+              options={{
+                strokeColor: '#7721AD',
+                strokeOpacity: 0.8,
                 strokeWeight: 2,
-                clickable: true,
-                draggable: true,
-                editable: true,
-                visible: true,
-            }}
+                fillColor: '#7721AD',
+                fillOpacity: 0.35,
+              }}
+              onDragEnd={updatePolygonCoords}
+              onMouseUp={updatePolygonCoords}
             />
+          )}
         </GoogleMap>
-    );
+      </LoadScript>
+    </div>
+  );
 };
 
 export default GoogleMaps;
--- src/pages/deliveryorderslist/views/detailTabs/DeliveryAddressTabDetail.tsx
+++ src/pages/deliveryorderslist/views/detailTabs/DeliveryAddressTabDetail.tsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Dictionary } from "@types";
-import { makeStyles } from "@material-ui/core/styles";
-import { useTranslation } from "react-i18next";
-import { langKeys } from "lang/keys";
-import { FieldErrors, UseFormGetValues, UseFormSetValue } from "react-hook-form";
-import { FieldEdit, TitleDetail } from "components";
-import { Typography } from "@material-ui/core";
+import React, { useState, useEffect } from "react"; 
+import { makeStyles } from "@material-ui/core/styles"; 
+import { useTranslation } from "react-i18next"; 
+import { langKeys } from "lang/keys"; 
+import { FieldErrors } from "react-hook-form"; 
+import { FieldEdit } from "components"; 
 import GoogleMaps from "components/fields/GoogleMapsPrueba";
 
-const default_latitude1 = -12.00000000000001; const default_longitude1 = -77.00000000000001; //coo sup izq
-const default_latitude2 = -12.00200000000001; const default_longitude2 = -77.00000000000001; //coo sup der
-const default_latitude3 = -12.00200000000001; const default_longitude3 = -77.00200000000001; //coo inf izq
-const default_latitude4 = -12.00000000000001; const default_longitude4 = -77.00200000000001; //coo inf der
+const default_latitude1 = -12.00000000000001; const default_longitude1 = -77.00000000000001; 
+const default_latitude2 = -12.00200000000001; const default_longitude2 = -77.00000000000001; 
+const default_latitude3 = -12.00200000000001; const default_longitude3 = -77.00200000000001; 
+const default_latitude4 = -12.00000000000001; const default_longitude4 = -77.00200000000001; 
+
 const fixedDifference = 0.002;
 
 const useStyles = makeStyles((theme) => ({
@@ -19,24 +18,10 @@ const useStyles = makeStyles((theme) => ({
         marginTop: theme.spacing(2),
         padding: theme.spacing(2),
         background: "#fff",
-    },
-    subtittle: {
-        textAlign: "left",
-        padding: "2rem 2rem 2rem 0",
-        fontSize: "1.3rem",
-    }, 
-    maptext: {
-        textAlign: "center",
-        padding: "1rem 0rem 3rem 0",
-        fontSize: "1rem",
-        color: "gray",
-    },
+    },   
 }));
 
-interface InventoryTabDetailProps {
-    row: Dictionary | null;
-    setValue: UseFormSetValue<any>;
-    getValues: UseFormGetValues<any>;
+interface InventoryTabDetailProps {   
     errors: FieldErrors;
 }
 
@@ -44,7 +29,7 @@ interface Coordinates {
     points: { lat: number; lng: number }[];
 }
 
-const DeliveryAddressTabDetail: React.FC<InventoryTabDetailProps> = ({ row, setValue, getValues, errors}) => {
+const DeliveryAddressTabDetail: React.FC<InventoryTabDetailProps> = ({ errors}) => {
     const { t } = useTranslation();
     const classes = useStyles();
  
@@ -66,6 +51,8 @@ const DeliveryAddressTabDetail: React.FC<InventoryTabDetailProps> = ({ row, setV
             { lat: default_latitude4 || 0, lng: default_longitude4 || 0 },
         ],
     });
+
+    
 
     const handleMapClick = (e: { lat: number; lng: number }) => {
 
@@ -104,16 +91,8 @@ const DeliveryAddressTabDetail: React.FC<InventoryTabDetailProps> = ({ row, setV
 
 
     return (
-    <div className={classes.containerDetail}>
+    <div className={classes.containerDetail}>    
         <div className="row-zyx">
-            <TitleDetail title={t(langKeys.deliveryaddress)} />
-        </div>
-
-        <Typography className={classes.subtittle}>
-            {t(langKeys.geolocation)}
-        </Typography>
-
-        <div className="row-zyx" style={{ paddingBottom: "1rem" }}>
             <FieldEdit
                 label={`${t(langKeys.latitude)}: Coor 1`}
                 type="number"
@@ -172,10 +151,7 @@ const DeliveryAddressTabDetail: React.FC<InventoryTabDetailProps> = ({ row, setV
             />
         </div>
         <div className="row-zyx" style={{ justifyContent: "center" }}>
-            <GoogleMaps coordinates={googleMapsCoordinates} onMapClick={handleMapClick} />
-            <Typography className={classes.maptext}>
-                {t(langKeys.address_found_in_geolocator)}
-            </Typography>
+            <GoogleMaps />  
         </div>
     </div>
     );
