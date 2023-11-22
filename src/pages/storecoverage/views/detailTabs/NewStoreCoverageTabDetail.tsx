8bd863d2dbced5aac09af3cd943be794b2fab53b
nirvaGarciaV <nirvana.garcia@vcaperu.com>
2023-11-22 16:21:42 -0500

    adding map on store coverage area

--- src/pages/storecoverage/views/detailTabs/NewStoreCoverageTabDetail.tsx
+++ src/pages/storecoverage/views/detailTabs/NewStoreCoverageTabDetail.tsx
@@ -1,19 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useTranslation } from "react-i18next";
 import { langKeys } from "lang/keys";
 import { FieldEdit, FieldSelect, IOSSwitch } from 'components';
 import { FieldErrors } from "react-hook-form";
-import { FormControlLabel } from "@material-ui/core";
+import { Button, FormControlLabel, IconButton, Typography } from "@material-ui/core";
 import { useSelector } from 'hooks';
 import { Dictionary } from "@types";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
+import GoogleMaps, {IsOrderCoordinateInsidePolygon } from 'components/fields/GoogleMaps';
 
 const useStyles = makeStyles((theme) => ({
-  containerDetail: {
-    marginTop: theme.spacing(2),
-    padding: theme.spacing(2),
-    background: "#fff",
-  }, 
+    containerDetail: {
+        marginTop: theme.spacing(2),
+        padding: theme.spacing(2),
+        background: "#fff",
+    }, 
+    subtitle: {
+        fontSize: '1.2rem',   
+        margin: '2rem 0'   
+    },
+    mapFooter: {
+        fontSize: '1rem',   
+        color: 'grey',
+        textAlign: 'center', 
+    },
+    button: {
+        display: "flex",
+        gap: "10px",
+        alignItems: "center",        
+    },   
+    addbutton: {
+    backgroundColor: "#55BD84",
+        margin: "0 0 1rem 0",      
+        '&:hover': {
+            backgroundColor: '#55BD84',
+            borderRadius: 4
+        }
+    },
 }));
 
 interface NewOrderTabDetailProps {
@@ -24,15 +49,40 @@ interface NewOrderTabDetailProps {
 }
 
 const NewStoreCoverageTabDetail: React.FC<NewOrderTabDetailProps> = ({ errors, row, setValue, getValues}) => {
-  const { t } = useTranslation();
-  const classes = useStyles();
-  const [inStore, setInStore] = useState(row?.warehouseinstore || false);
-  const multiData = useSelector(state => state.main.multiData);
+    const { t } = useTranslation();
+    const classes = useStyles();
+    const [inStore, setInStore] = useState(row?.warehouseinstore || false);
+    const multiData = useSelector(state => state.main.multiData);
 
-  function handleSwitchChange (event: React.ChangeEvent<HTMLInputElement>) {
-    setValue('warehouseinstore', event.target.checked)
-    setInStore(event.target.checked)
-  }
+    function handleSwitchChange (event: React.ChangeEvent<HTMLInputElement>) {
+        setValue('warehouseinstore', event.target.checked)
+        setInStore(event.target.checked)
+    }
+
+    /*INICIO AGREGACIÓN Mapa*/
+    const [coordinates, setCoordinates] = useState<Array<{ latitude: number; longitude: number }>>([
+        { latitude: -12.00000000000001, longitude: -77.00000000000001 },
+    ]);
+    const handleAddCoordinate = () => {
+    setCoordinates((prevCoordinates) => [...prevCoordinates, { latitude: 0, longitude: 0 }]);
+    };
+    const handleDeleteCoordinate = (index: number) => {
+    setCoordinates((prevCoordinates) => prevCoordinates.filter((_, i) => i !== index));
+    };
+    const handleCoordinatesChange = (newCoordinates: Array<{ latitude: number; longitude: number }>) => {
+    setCoordinates(newCoordinates);
+    };
+
+    const [orderCoordinate, setOrderCoordinate] = useState<{ latitude: number; longitude: number }>({ latitude: 0, longitude: 0 });
+    const [isOrderInsidePolygon, setIsOrderInsidePolygon] = useState<boolean | null>(null);
+    useEffect(() => {
+        const isInside = IsOrderCoordinateInsidePolygon(
+            { lat: orderCoordinate.latitude, lng: orderCoordinate.longitude },
+            coordinates.map(coord => ({ lat: coord.latitude, lng: coord.longitude }))
+        );
+        setIsOrderInsidePolygon(isInside);
+    }, [orderCoordinate, coordinates]);    
+    /*FIN AGREGACIÓN Mapa*/
 
   return (
     <div className={classes.containerDetail}>
@@ -61,14 +111,7 @@ const NewStoreCoverageTabDetail: React.FC<NewOrderTabDetailProps> = ({ errors, r
             onChange={(value) => setValue('address', value)}
             className="col-6"
             error={typeof errors?.address?.message === 'string' ? errors?.address?.message : ''}
-        />
-        <FieldEdit
-            label={t(langKeys.coveragearea)}
-            valueDefault={getValues('coveragearea')}
-            onChange={(value) => setValue('coveragearea', value)}
-            className="col-6"
-            error={typeof errors?.coveragearea?.message === 'string' ? errors?.coveragearea?.message : ''}
-        />
+        />      
         <FieldSelect
           label={t(langKeys.status)}
           className="col-6"
@@ -102,7 +145,110 @@ const NewStoreCoverageTabDetail: React.FC<NewOrderTabDetailProps> = ({ errors, r
           />}
           label={t(langKeys.instorewarehouse)}
           className="col-5"
-        />       
+        /> 
+       
+
+        <div /*agregacion mapa*/>
+        <Typography className={classes.subtitle}>{t(langKeys.coveragearea)}</Typography>
+        <div className="row-zyx">
+            <FieldEdit
+                label={`${t(langKeys.latitude)}`}
+                type="number"
+                className="col-6"
+                valueDefault={orderCoordinate.latitude}
+                onChange={(newValue) => {
+                    setOrderCoordinate({
+                        ...orderCoordinate,
+                        latitude: Number(newValue),
+                    });
+                }}
+            />
+            <FieldEdit
+                label={`${t(langKeys.longitude)}`}
+                type="number"
+                className="col-6"
+                valueDefault={orderCoordinate.longitude}
+                onChange={(newValue) => {
+                    setOrderCoordinate({
+                        ...orderCoordinate,
+                        longitude: Number(newValue),
+                    });
+                }}
+            />
+            <FieldEdit
+                label={t(langKeys.message)}
+                type="text"
+                disabled={true}
+                className="col-12"
+                valueDefault={
+                    isOrderInsidePolygon !== null
+                        ? isOrderInsidePolygon
+                            ? 'Coordenadas dentro del polígono.'
+                            : 'Coordenadas fuera del polígono.'
+                        : 'Cargando...'
+                }
+            />
+        </div>
+        <div className="row-zyx" style={{ justifyContent: 'center' }}>
+        <GoogleMaps 
+            coordinates={coordinates} 
+            onCoordinatesChange={handleCoordinatesChange} 
+        />
+        </div>
+        <Typography className={classes.mapFooter}>{t(langKeys.address_found_in_geolocator)}</Typography>
+        <div style={{ textAlign: "right" }}>    
+        <Button
+            className={classes.addbutton}
+            variant="contained"
+            type="button"
+            color="primary"
+            startIcon={<AddIcon color="secondary" />}
+            onClick={handleAddCoordinate}
+        >
+            {t(langKeys.add) + " " + t(langKeys.coordinate)}
+        </Button>
+        </div>
+        {coordinates.map((coord, index) => (
+        <div key={index} className="row-zyx">
+            <div className="col-1">
+            <IconButton onClick={() => handleDeleteCoordinate(index)}>
+                <DeleteIcon />
+            </IconButton>
+            </div>
+            <div className="col-5">
+            <FieldEdit
+                label={`${t(langKeys.latitude)} ${index + 1}`}
+                type="number"
+                valueDefault={coord.latitude}
+                onChange={(newValue) => {
+                setCoordinates((prevCoordinates) =>
+                    prevCoordinates.map((prevCoord, i) =>
+                    i === index ? { ...prevCoord, latitude: Number(newValue) } : prevCoord
+                    )
+                );
+                }}
+            />
+            </div>
+            <div className="col-5">
+            <FieldEdit
+                label={`${t(langKeys.longitude)} ${index + 1}`}
+                type="number"
+                valueDefault={coord.longitude}
+                onChange={(newValue) => {
+                setCoordinates((prevCoordinates) =>
+                    prevCoordinates.map((prevCoord, i) =>
+                    i === index ? { ...prevCoord, longitude: Number(newValue) } : prevCoord
+                    )
+                );
+                }}
+            />
+            </div>
+        </div>
+        ))}
+
+
+        </div>
+          
       </div>
     </div>
   );
