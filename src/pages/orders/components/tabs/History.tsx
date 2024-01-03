e370350db1936fe500e0e4c11e9172687b374c59
nirvanaGarciaV <nirvana.garcia@vcaperu.com>
2024-01-03 12:25:53 -0500

    fixing history order

--- src/pages/orders/components/tabs/History.tsx
+++ src/pages/orders/components/tabs/History.tsx
@@ -1,86 +1,84 @@
-import React from 'react'; // we need this to make JSX compile
-import { useSelector } from 'hooks';
-import { convertLocalDate } from 'common/helpers';
-import { Dictionary } from "@types";
-import TableZyx from 'components/fields/table-simple';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { convertLocalDate } from 'common/helpers';
 import { useTranslation } from 'react-i18next';
 import { langKeys } from 'lang/keys';
-import { CellProps } from 'react-table';
 
 const useStyles = makeStyles((theme) => ({
-    containerDetail: {
-        marginTop: theme.spacing(2),
-        padding: theme.spacing(2),
-        background: '#fff',
-    },
+  timelineItem: {
+    position: 'relative', 
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spacing(5),
+    paddingLeft: theme.spacing(8),
+  },
+  timelineCircle: {
+    position: 'absolute',
+    left: '0', 
+    top: '50%',
+    transform: 'translateY(-50%)',
+    width: '40px',
+    height: '40px',
+    borderRadius: '50%',
+    backgroundColor: '#7721AD', 
+  },
+  descriptionContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  description: {
+    fontSize: '1.4rem',
+    marginRight: theme.spacing(2),
+  },
+  createdAt: {
+    fontSize: '1rem',
+  },
+  actionBy: {
+    fontSize: '1rem',
+  },
+  containerDetail: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(2),
+    background: '#fff',
+  },
+  
 }));
 
-interface MultiData {
-    data: Dictionary[];
-    success: boolean;
-}
-
-interface HistoryProps {  
-    multiData: MultiData[];
+interface HistoryProps {
+  multiData: any;
 }
 
 const History: React.FC<HistoryProps> = ({ multiData }) => {
-    const classes = useStyles();
-    const { t } = useTranslation();
-    const mainResult = useSelector(state => state.main);
-    const datahistory = multiData[1] && multiData[1].success ? multiData[1].data : [];
+  const classes = useStyles();
+  const { t } = useTranslation();
+  const datahistory = multiData[1] && multiData[1].success ? multiData[1].data : [];
 
-    const columnsHistory = React.useMemo(
-        () => [
-            {
-                Header: t(langKeys.description),
-                accessor: 'col_description',
-                width: 'auto',
-                Cell: (props: CellProps<Dictionary>) => {
-                    const { description } = props.cell.row.original;
-                    return <div style={{ padding: 16 }}>{t(description.toLowerCase())}</div>
-                },            
-            },
-            {
-                Header: t(langKeys.createdBy),
-                accessor: 'createby',
-                width: 'auto',
-                Cell: (props: CellProps<Dictionary>) => {
-                    const { createby } = props.cell.row.original;
-                    return <div style={{ padding: 16 }}>{createby}</div>
-                }
-            },
-            {
-                Header: t(langKeys.date),
-                accessor: 'changedate',
-                width: 'auto',
-                type: 'date',
-                Cell: (props: CellProps<Dictionary>) => {
-                    const row = props.cell.row.original;
-                    return convertLocalDate(row.createdate).toLocaleString()
-                }
-            },
-        ],
-        []
-    );
+  return (
+    <div className={classes.containerDetail}>
+        {datahistory.map((historyItem: any) => (
+            <div key={historyItem.orderhistoryid} className={classes.timelineItem}>
 
-    return (     
-        <>
-            <div className={classes.containerDetail}>
-                <div className="row-zyx">
-                    <TableZyx
-                        columns={columnsHistory}
-                        data={datahistory}
-                        loading={mainResult.multiData.loading}
-                        toolsFooter={false}
-                        filterGeneral={false}
-                    />
+                <div className={classes.timelineCircle} />
+                <div style={{display:'block'}}>
+                    <div className={classes.descriptionContainer}>
+                        <div className={classes.description}>
+                            {t(historyItem.description.toLowerCase())}
+                        </div>
+                        <div className={classes.createdAt}>
+                            {convertLocalDate(historyItem.createdate).toLocaleString()}
+                        </div>
+                    </div>
+
+                    <div className={classes.actionBy}>
+                        {t(langKeys.createdBy)}: {historyItem.createby}
+                    </div>
                 </div>
+               
+
             </div>
-          
-        </>
-    );
-}
+        ))}
+    </div>
+  );
+};
 
 export default History;
