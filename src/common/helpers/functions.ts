db8dd0c92b62fd6835657bbb6e40043b9936bc9f
Pablo Rojas <projasb25@gmail.com>
2024-01-24 16:56:51 -0500

    hex color from user

--- src/common/helpers/functions.ts
+++ src/common/helpers/functions.ts
@@ -1221,3 +1221,12 @@ export const loadScripts = (scripts: string[]) => {
 
     return { scriptPlatform, scriptRecaptcha, clarityScript, gtmScript }
 }
+
+export function hash256 (message: string) {
+    const msgBuffer = new TextEncoder().encode(message);
+    return crypto.subtle.digest('SHA-256', msgBuffer).then(hashBuffer => {
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+    });
+}
