c315c0855b065afeb4e73307335f8be3ff1db4f9
jose.vara <jose.vara@vcaperu.com>
2024-02-13 14:44:58 -0500

    RLA465. Calendarización vía módulo de oportunidades

--- src/lang/es.ts
+++ src/lang/es.ts
@@ -3444,6 +3444,7 @@ Carrousel: Carrusel utilizado por el canal Chatweb/Messenger `,
   invitation_date: "{{day}} {{date}} de {{month}} de {{year}}",
 
   type_activitylead_call: 'Llamada',
+  type_activitylead_appointment: 'CITA (Calendarización)',
   'type_activitylead_call for demo': 'Demo de llamada',
   'type_activitylead_email: welcome demo': 'Correo: Demostración de bienvenida',
   'type_activitylead_follow-up quote': 'Cotización de seguimiento',
