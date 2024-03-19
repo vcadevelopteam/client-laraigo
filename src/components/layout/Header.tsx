52d67f1442b308406a3d3110dc79d275ae9e7067
jose.vara <jose.vara@vcaperu.com>
2024-03-19 14:17:14 -0500

    header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -240,13 +240,15 @@ const Header = ({ classes }: IProps) => {
     const openDrawer = useSelector(state => state.popus.openDrawer);
     const user = useSelector(state => state.login.validateToken.user);
     const customDomain = !notCustomUrl.some(url => window.location.href.includes(url));
-    
-    if(customDomain){
-        const existingFavicon = document.querySelector('link[rel="icon"]');
-        if(user?.iconurl && existingFavicon) existingFavicon.href = user?.iconurl||"";
-        const str = user?.corpdesc || "Laraigo"
-        document.title = str.charAt(0).toUpperCase() + str.slice(1).toLocaleLowerCase();
-    }
+
+    useEffect(() => {
+        if(customDomain){
+            const existingFavicon = document.querySelector('link[rel="icon"]');
+            if(user?.iconurl && existingFavicon) existingFavicon.href = user?.iconurl||"";
+            const str = user?.corpdesc || "Laraigo"
+            document.title = str.charAt(0).toUpperCase() + str.slice(1).toLocaleLowerCase();
+        }
+    }, [window.location])
 
     return (
         <AppBar
