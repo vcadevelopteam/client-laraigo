6606f00c1f7386f5f9dd4d8ee2c6024a43c80562
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2024-03-20 14:13:59 -0500

    package-lock.json

---
+++ src/pages/PhoneRedirect.tsx
@@ -0,0 +1,38 @@
+import CircularProgress from "@material-ui/core/CircularProgress";
+import React, { FC, useEffect } from "react";
+
+import { makeStyles } from "@material-ui/core";
+import { useParams } from "react-router";
+
+const useStyles = makeStyles(() => ({
+    back: {
+        backgroundColor: "#fbfcfd",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+}));
+
+export const PhoneRedirect: FC = () => {
+    const classes = useStyles();
+
+    const { phone }: any = useParams();
+
+    useEffect(() => {
+        console.log('Opening phone: ' + phone);
+
+        window.open(`tel:${phone}`);
+        window.open("", "_self");
+        window.close();
+    }, [])
+
+    return (
+        <div className={classes.back}>
+            <CircularProgress />
+        </div>
+    );
+};
+
+export default PhoneRedirect;
