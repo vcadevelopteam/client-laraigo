534f0f229762ce65f03b798dae7108aed7e5337b
jose.vara <jose.vara@vcaperu.com>
2024-03-20 18:01:24 -0500

    a

--- src/pages/ReassignmentRules.tsx
+++ src/pages/ReassignmentRules.tsx
@@ -73,7 +73,7 @@ const DetailReassignmentRules: React.FC<DetailReassignmentRulesProps> = ({ data:
     React.useEffect(() => {
         register('type');
         register('id');
-        register('description', { validate: (value) => (value && value.length) || t(langKeys.field_required) });
+        register('description');
         register('group', { validate: (value) => (value && value.length) || t(langKeys.field_required) });
         register('assignedgroup', { validate: (value) => (value && value.length) || t(langKeys.field_required) });
         register('status', { validate: (value) => (value && value.length) || t(langKeys.field_required) });
