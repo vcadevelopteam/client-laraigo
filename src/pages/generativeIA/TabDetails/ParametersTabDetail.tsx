d8af8d8898c376c200bf907d3a06365a24f4e1b8
Sebas-Delgado <ronald.delgado@vcaperu.com>
2024-04-23 13:12:26 -0500

    language parameter in ai assistant creation fixed

--- src/pages/generativeIA/TabDetails/ParametersTabDetail.tsx
+++ src/pages/generativeIA/TabDetails/ParametersTabDetail.tsx
@@ -384,7 +384,13 @@ const ParametersTabDetail: React.FC<ParametersTabDetailProps> = ({
                                     label={t(langKeys.language)}
                                     data={languages}
                                     valueDefault={getValues('language')}
-                                    onChange={(value) => setValue('language', value)}
+                                    onChange={(value) => {
+                                        if(value) {
+                                            setValue('language', value.domainvalue)
+                                        } else {
+                                            setValue('language', '')
+                                        }
+                                    }}
                                     error={errors?.language?.message}
                                     optionValue='domainvalue'
                                     optionDesc='domaindesc'
@@ -580,8 +586,14 @@ const ParametersTabDetail: React.FC<ParametersTabDetailProps> = ({
                                 <FieldSelect
                                     label={t(langKeys.language)}
                                     data={languages}
-                                    valueDefault={selectedCardData?.language}
-                                    onChange={(value) => setValue('language', value)}
+                                    valueDefault={getValues('language')}
+                                    onChange={(value) => {
+                                        if(value) {
+                                            setValue('language', value.domainvalue)
+                                        } else {
+                                            setValue('language', '')
+                                        }
+                                    }}
                                     optionValue='domainvalue'
                                     optionDesc='domaindesc'
                                 />
