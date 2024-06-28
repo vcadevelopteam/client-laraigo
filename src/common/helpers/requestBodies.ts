50b49b39cde10cc0a110fffba580040f1ca8d302
Carlos Farro <carlos.farro@vcaperu.com>
2024-06-28 11:25:48 -0500

    fix save campaign start with memberscount

--- src/common/helpers/requestBodies.ts
+++ src/common/helpers/requestBodies.ts
@@ -1431,7 +1431,7 @@ export const insCampaign = ({
     carouseljson,
     variableshidden, 
     selectedColumns
-}: Dictionary): IRequestBody => ({
+}: Dictionary, membercount: number = 0): IRequestBody => ({
     method: "UFN_CAMPAIGN_INS",
     parameters: {
         id,
@@ -1464,6 +1464,8 @@ export const insCampaign = ({
         operation,
         carouseljson: JSON.stringify(carouseljson),
         variableshidden: JSON.stringify(variableshidden),
+        membercount,
+        offset: (new Date().getTimezoneOffset() / 60) * -1
     }
 });
 
