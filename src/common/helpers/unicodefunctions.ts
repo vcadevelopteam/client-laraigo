83277076177dd386334e5a97e3cd9ec03bee3721
JoseVara <jose.vara@vcaperu.com>
2024-08-15 16:19:23 -0500

    fix

--- src/common/helpers/unicodefunctions.ts
+++ src/common/helpers/unicodefunctions.ts
@@ -2,16 +2,16 @@ type StyleMap = { [key: string]: string };
 import {string_to_unicode_variant} from "string-to-unicode-variant";
 
 export function formatTextToUnicode({text = '', bold = false, italic = false, underline = false, strikethrough = false, monospaced = false} = {}) {    
-    let variant = ""
+    let variant = []
     let combinings = []
     if (monospaced) {
-        variant += "m "
+        variant.push("monospace")
     }
     if (bold) {
-        variant += "b "
+        variant.push("bold")
     }
     if (italic) {
-        variant += "i "
+        variant.push("italic")
     }
     if (underline) {
         combinings.push("underline")
@@ -19,7 +19,7 @@ export function formatTextToUnicode({text = '', bold = false, italic = false, un
     if (strikethrough) {
         combinings.push("strike")
     }
-    return string_to_unicode_variant(text, variant.trim(), combinings);
+    return string_to_unicode_variant(text, variant, combinings);
 }
 
 export const removeUnicodeStyle = (text: string): string => {
