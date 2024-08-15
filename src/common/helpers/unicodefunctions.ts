ea06570f5693246e7d724660c20d737c1e522463
JoseVara <jose.vara@vcaperu.com>
2024-08-15 16:15:56 -0500

    minifix

--- src/common/helpers/unicodefunctions.ts
+++ src/common/helpers/unicodefunctions.ts
@@ -4,20 +4,20 @@ import {string_to_unicode_variant} from "string-to-unicode-variant";
 export function formatTextToUnicode({text = '', bold = false, italic = false, underline = false, strikethrough = false, monospaced = false} = {}) {    
     let variant = ""
     let combinings = []
+    if (monospaced) {
+        variant += "m "
+    }
     if (bold) {
-        variant += "bold "
+        variant += "b "
     }
     if (italic) {
-        variant += "italic "
+        variant += "i "
     }
     if (underline) {
         combinings.push("underline")
     }
     if (strikethrough) {
         combinings.push("strike")
-    }
-    if (monospaced) {
-        variant += "monospace"
     }
     return string_to_unicode_variant(text, variant.trim(), combinings);
 }
