690270cbec2b0ed868459abbc3d063da1fae3672
JoseVara <jose.vara@vcaperu.com>
2024-08-27 11:21:30 -0500

    ILA181

--- src/pages/Tipifications.tsx
+++ src/pages/Tipifications.tsx
@@ -751,17 +751,6 @@ const Tipifications: FC = () => {
                 channels: !!item.channels?String(item.channels).replace(/\s+/g, '').replace(/;/g, ','):item.channels,
                 type: (item.type.toLowerCase() === "clasificación" || item.type.toLowerCase() === "clasificacion") ? 'TIPIFICACION': ((item.type.toLowerCase() === "categoría" || item.type.toLowerCase() === "categoria")?"CATEGORIA":item.type),
             }));
-            data=data.filter((d: any) => {
-                const channelList = filteredChannels.map((x: any)=>x.domainvalue)
-                const hasValidClassification = d.classification !== '' && d.classification != null;
-                const hasValidChannels = d.type === "TIPIFICACION"?d.channels !== '' && d.channels != null && d.channels.split(',').every((channel:any) => channelList.includes(channel)):true
-                const hasValidType = d.type === "TIPIFICACION" || d.type === "CATEGORIA"
-                const parentExists = ['', null, undefined].includes(d.parent) || 
-                    Object.keys(mainResult.multiData.data[1].data.reduce((acc, item) => ({ ...acc, [item.classificationid]: item.title }), {0: ''}))
-                    .includes(String(d.parent));
-            
-                return hasValidClassification && hasValidChannels && parentExists && hasValidType;
-            });   
             const invaliddata = data.filter((d: any) => {
                 const channelList = filteredChannels.map((x: any)=>x.domainvalue)
                 const hasValidClassification = d.classification !== '' && d.classification != null;
@@ -772,6 +761,17 @@ const Tipifications: FC = () => {
                     .includes(String(d.parent));
             
                 return !hasValidClassification || !hasValidChannels || !parentExists || !hasValidType;
+            });   
+            data=data.filter((d: any) => {
+                const channelList = filteredChannels.map((x: any)=>x.domainvalue)
+                const hasValidClassification = d.classification !== '' && d.classification != null;
+                const hasValidChannels = d.type === "TIPIFICACION"?d.channels !== '' && d.channels != null && d.channels.split(',').every((channel:any) => channelList.includes(channel)):true
+                const hasValidType = d.type === "TIPIFICACION" || d.type === "CATEGORIA"
+                const parentExists = ['', null, undefined].includes(d.parent) || 
+                    Object.keys(mainResult.multiData.data[1].data.reduce((acc, item) => ({ ...acc, [item.classificationid]: item.title }), {0: ''}))
+                    .includes(String(d.parent));
+            
+                return hasValidClassification && hasValidChannels && parentExists && hasValidType;
             });   
             setInvalidImportData(invaliddata)    
             if (data.length > 0) {
@@ -785,8 +785,8 @@ const Tipifications: FC = () => {
                         tags: x.tag || '',
                         parent: x.parent || 0,
                         operation: "INSERT",
-                        oder: x.type === "TIPIFICACION" ? '':"1",
-                        status: "ACTIVO",
+                        order: x.type === "TIPIFICACION" ? '':"1",
+                        status: x?.status || "ACTIVO",
                         id: 0,
                     })
                     return ad;
